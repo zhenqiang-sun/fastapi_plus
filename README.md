417b1d2462a4cc22b79f3d68b6c1a84b2045959c
zhenqiang.sun <zhenqiang.sun@qq.com>
2020-09-01 10:29:44 +0800

    修改配置文件

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 #Fastapi Plus
 这是一个Python FastAPI项目工程库，包含DB、Redis、MongoDB、JSON等工具和基础服务类。
+
+
+
+### 组件：
+- FastAPI: https://fastapi.tiangolo.com/
+- uvicorn: https://www.uvicorn.org/
+- SQLAlchemy: https://www.sqlalchemy.org/
+- PyMySQL: https://pymysql.readthedocs.io/
+- REDIS: https://github.com/andymccurdy/redis-py
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-FastAPI Utils
+FastAPI Plus
 """
 
 from setuptools import find_packages, setup
@@ -11,7 +11,7 @@ setup(
     author_email="zhenqiang.sun@qq.com",
     description='FastAPI项目工程库',
     long_description='这是一个Python FastAPI项目工程库，包含DB、Redis、MongoDB、JSON等工具和基础服务类。',
-    url="https://gitee.com/erlitech/fastapi_plus",
+    url="https://github.com/zhenqiang-sun/fastapi_plus",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
@@ -21,8 +21,18 @@ setup(
         "Operating System :: OS Independent",
     ],
 
-    install_requires=['fastapi', 'uvicorn', 'sqlalchemy', 'pymysql', 'sqlacodegen', 'redis', 'pymongo', 'requests',
-                      'python-multipart'],
+    install_requires=[
+        'fastapi==0.61.1',
+        'uvicorn==0.11.8',
+        'SQLAlchemy==1.3.19',
+        'PyMySQL==0.10.0',
+        'sqlacodegen==2.3.0',
+        'redis==3.5.3',
+        'pymongo==3.11.0',
+        'requests==2.24.0',
+        'python-multipart==0.0.5',
+        'aiofiles==0.5.0'
+    ],
 
     # List additional groups of dependencies here
     extras_require={},
