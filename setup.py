a814722b7883f8ac0b7444d43dd2d9cfd02149d5
zhenqiang.sun <zhenqiang.sun@qq.com>
2020-11-18 12:57:13 +0800

    修改版本及依赖库

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="fastapi_plus",
-    version='0.0.5.20201118',
+    version='0.1.0.20201118',
     author="Zhenqiang Sun",
     author_email="zhenqiang.sun@gmail.com",
     description="This is a Python FastAPI project engineering library that includes tools and basic service classes.",
@@ -24,15 +24,15 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'fastapi==0.61.1',
-        'uvicorn==0.11.8',
-        'SQLAlchemy==1.3.19',
+        'fastapi==0.61.2',
+        'uvicorn==0.12.2',
+        'sqlalchemy==1.3.19',
         'PyMySQL==0.10.0',
         'sqlacodegen==2.3.0',
         'redis==3.5.3',
-        'pymongo==3.11.0',
-        'requests==2.24.0',
+        'pymongo==3.11.1',
+        'requests==2.25.0',
         'python-multipart==0.0.5',
-        'aiofiles==0.5.0'
+        'aiofiles==0.6.0'
     ],
 )
